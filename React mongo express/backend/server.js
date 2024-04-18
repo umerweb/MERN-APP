6881d74f14c0b4d5d9cfecc504cac710569bc710
umer.web <umer.web2@gmail.com>
2024-04-19 02:07:22 +0500

    first commit

---
+++ React mongo express/backend/server.js
@@ -0,0 +1,55 @@
+import express from "express";
+import 'dotenv/config'
+import { MongoClient } from "mongodb";
+import bodyParser from "body-parser";
+import cors from 'cors'
+
+console.log(process.env.mongo_uri) // remove this after you've confirmed it is working
+
+
+const url = 'mongodb://localhost:27017';
+
+const client = new MongoClient(url)
+
+const dbname = 'passop';
+
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+const port = 3000
+
+client.connect();
+
+const db = client.db(dbname);
+
+
+// Get all passwords
+app.get('/', async (req, res) => {
+  const collection = db.collection('passwords');
+  const findresults = await collection.find({}).toArray();
+  res.json(findresults)
+})
+// SEnd Passwords
+
+app.post('/', async (req, res) => {
+  const data = req.body
+  const collection = db.collection('passwords');
+  const senddata = await collection.insertOne(data)
+  res.send({success: true, senddata})
+})
+
+
+//Delete Passwords
+
+app.delete('/', async (req, res) => {
+  const data = req.body
+  const collection = db.collection('passwords');
+  const senddata = await collection.deleteOne(data)
+  res.send({success: true, senddata})
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
